34694e372f7a06f7ff0b2f255d246d72d9a91106
Георгий Пьянков <piankov@MacBook-Air-Georgij.local>
2020-05-08 13:53:44 +0300

    init

---
+++ train.py
@@ -0,0 +1,75 @@
+import sys
+import numpy as np
+import os
+import agent
+
+print(os.getcwd())
+
+def formatPrice(n):
+	return ("-" if n < 0 else "") + "{0:.2f}".format(abs(n))
+
+# returns the vector containing stock data from a fixed file
+def getStockDataVec(key):
+	vec = []
+	lines = open("data/" + key + ".csv", "r").read().splitlines()
+
+	for line in lines[1:]:
+		vec.append(float(line.split(",")[4]))
+	return vec
+
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
+def getState(data, t, n):
+	d = t - n + 1
+	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
+	res = []
+	for i in range(n - 1):
+		res.append(sigmoid(block[i + 1] - block[i]))
+
+	return np.array([res])
+
+
+
+stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
+
+agent = agent.Agent(window_size)
+data = getStockDataVec(stock_name)
+l = len(data) - 1
+batch_size = 32
+
+for e in range(episode_count + 1):
+	print("Эпоха " + str(e))
+	state = getState(data, 0, window_size + 1)
+
+	total_profit = 0
+	agent.inventory = []
+
+	for t in range(l):
+		action = agent.act(state)
+
+		# sit
+		next_state = getState(data, t + 1, window_size + 1)
+		reward = 0
+
+		if action == 1: # buy
+			agent.inventory.append(data[t])
+			print("Покупка: " + formatPrice(data[t]))
+
+		elif action == 2 and len(agent.inventory) > 0: # sell
+			bought_price = agent.inventory.pop(0)
+			reward = max(data[t] - bought_price, 0)
+			total_profit += data[t] - bought_price
+			print("Продажа: " + formatPrice(data[t]) + " | Прибыль: " + formatPrice(data[t] - bought_price))
+
+		done = True if t == l - 1 else False
+		agent.memory.append((state, action, reward, next_state, done))
+		state = next_state
+
+		if done:
+			print ("Суммарная прибыль: " + formatPrice(total_profit))
+
+		if len(agent.memory) > batch_size:
+			agent.expReplay(batch_size)
+
+		agent.model.save("model_ep" + str(e))
